cb7f8d7d3eaf7714c02ba5796ce298768f995c02
Alex <bralix@mail.ru>
2019-02-08 03:42:11 +0300

    release v1

---
+++ stringMerge/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace stringMerge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(490, 146);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(165, 46);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Clear the fields";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label4.Location = new System.Drawing.Point(45, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 22);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Status";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.Color.RoyalBlue;
+            this.richTextBox4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.richTextBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox4.ForeColor = System.Drawing.Color.LightCyan;
+            this.richTextBox4.Location = new System.Drawing.Point(109, 159);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.Size = new System.Drawing.Size(345, 90);
+            this.richTextBox4.TabIndex = 17;
+            this.richTextBox4.Text = "Waiting for input ...";
+            this.richTextBox4.TextChanged += new System.EventHandler(this.richTextBox4_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Result";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(109, 117);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(363, 33);
+            this.richTextBox3.TabIndex = 15;
+            this.richTextBox3.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(490, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 45);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Extra  text";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 20);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Primary text";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(109, 66);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(363, 33);
+            this.richTextBox2.TabIndex = 11;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(109, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(363, 33);
+            this.richTextBox1.TabIndex = 10;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(490, 77);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 46);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Random Fill";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(677, 261);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "String Merge";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ stringMerge/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stringMerge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool success = false;
+            String defaultText = null;
+            String addedItem = "";
+            String[] wordDefault = new string[0];
+            String[] wordAdded = new string[0];
+            String result = "";
+
+            try
+            {
+                defaultText = richTextBox1.Text.Trim();
+                char[] charArrayDefault = defaultText.ToCharArray();
+                Array.Reverse(charArrayDefault);
+                defaultText = new string(charArrayDefault);
+
+                addedItem = richTextBox2.Text.Trim();
+                char[] charArrayAdd = addedItem.ToCharArray();
+                Array.Reverse(charArrayAdd);
+                addedItem = new string(charArrayAdd);
+
+                if (!String.IsNullOrEmpty(defaultText) && !String.IsNullOrEmpty(addedItem))
+                {
+
+
+                    if (!String.IsNullOrEmpty(defaultText))
+                    {
+                        wordDefault = defaultText.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        if (!String.IsNullOrEmpty(addedItem))
+                        {
+                            wordAdded = addedItem.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                            success = true;
+                        }
+                        else
+                        {
+                            richTextBox4.Text = "Incorrect input of the second field! Try again...";
+                            richTextBox4.BackColor = Color.Red;
+                        }
+                    }
+                    else
+                    {
+                        richTextBox4.Text = "Incorrect entry of the first field! Try again...";
+                        richTextBox4.BackColor = Color.Red;
+                    }
+                }
+                else
+                {
+                    richTextBox4.Text = "Invalid input! Try again...";
+                    richTextBox4.BackColor = Color.Red;
+                }
+
+                while ((wordDefault.Length > 0) && (wordAdded.Length > 0))
+                {
+                    result += wordDefault[0] + " ";
+                    result += wordAdded[0] + " ";
+
+                    wordDefault = wordDefault.Skip(1).ToArray();
+                    wordAdded = wordAdded.Skip(1).ToArray();
+                }
+
+                while (wordDefault.Length > 0)
+                {
+                    result += wordDefault[0] + " ";
+                    wordDefault = wordDefault.Skip(1).ToArray();
+                }
+
+                while (wordAdded.Length > 0)
+                {
+                    result += wordAdded[0] + " ";
+                    wordAdded = wordAdded.Skip(1).ToArray();
+                }
+
+                if (success)
+                {
+                    richTextBox3.Text = result.ToString();
+                    richTextBox4.BackColor = Color.Green;
+                    richTextBox4.Text = "Everything went well!";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox4.BackColor = Color.RoyalBlue;
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            richTextBox4.Clear();
+            richTextBox4.Text = "Waiting for input ...";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
